89241cb85b436b825e89d6891abee2032149f14b
dioph <diofanto.nunes@gmail.com>
2019-05-20 10:32:20 -0300

    wavelet analysis (from wavelets module)

--- periodicity/periodogram.py
+++ periodicity/periodogram.py
@@ -1,5 +1,6 @@
 import numpy as np
 from astropy.stats import LombScargle
+from wavelets import WaveletAnalysis
 
 
 def lombscargle(t, x, dx=None, f0=0, fmax=None, n=5, fap_method=None, fap_level=None):
@@ -76,8 +77,39 @@ def window(t, n=5):
     return f, a
 
 
-def wavelet(t, x):
-    raise NotImplementedError
-    pass
+def wavelet(t, x, pmin=0, pmax=None, n_periods=1000):
+    """Global Wavelet Power Spectrum using Morlet wavelets
+
+    Parameters
+    ----------
+    t: array-like
+        time array
+    x: array-like
+        signal array
+    pmin: float (optional default=0)
+        minimum period
+    pmax: float (optional default None)
+        maximum period; if None is given, uses default scaling
+    n_periods: int (optional default=1000)
+        number of trial periods
+
+    Returns
+    -------
+    wa: wavelets.WaveletAnalysis object
+        the full object for the given dataset
+    periods: array-like
+        trial periods array
+    gwps: array-like
+        Global Wavelet Power Spectrum (WPS projected on period axis)
+    wps: array-like
+        Wavelet Power Spectrum
+    """
+    dt = float(np.median(np.diff(t)))
+    wa = WaveletAnalysis(x, t, dt=dt, mask_coi=True, unbias=True)
+    periods = wa.fourier_periods
+    wps = wa.wavelet_power
+    gwps = wa.global_wavelet_spectrum
+    return wa, periods, gwps, wps
+
 
 # TODO: check out Supersmoother (Reimann 1994)
