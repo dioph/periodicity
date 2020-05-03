66bd7584a534d6be62e9ff471671162eb2338221
dioph <dioph@protonmail.com>
2020-05-03 17:29:31 -0300

    `setup.py` gets version from `__init__.py`

--- setup.py
+++ setup.py
@@ -1,20 +1,28 @@
+import re
+
 import setuptools
 
 with open("README.md", 'r') as f:
     long_description = f.read()
 
+version = re.search(
+    '^__version__\\s*=\\s*"(.*)"',
+    open('periodicity/__init__.py').read(),
+    re.M
+).group(1)
+
 setuptools.setup(
     name="periodicity",
-    version="0.1.0b3",
+    version=version,
     author="Eduardo Nunes",
-    author_email="diofanto.nunes@gmail.com",
+    author_email="dioph@pm.me",
     license="MIT",
     description="Useful tools for analysis of periodicities in time series data",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/dioph/periodicity",
     packages=setuptools.find_packages(),
-    install_requires=['numpy>=1.11', 'astropy>=1.3', 'scipy>=0.19.0',
+    install_requires=['numpy', 'astropy', 'scipy>=1.2.0',
                       'emcee', 'tqdm', 'autograd'],
     classifiers=(
         "Programming Language :: Python :: 3",
