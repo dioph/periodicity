5688c2fef441f9f24c5d2578ec4bd7cdd5e3abb1
dioph <dioph@protonmail.com>
2020-06-03 11:45:53 -0300

    Using multiprocessing for phase-folding methods

--- periodicity/phase.py
+++ periodicity/phase.py
@@ -1,10 +1,24 @@
 import numpy as np
 from scipy.ndimage import gaussian_filter1d
+from multiprocessing import Pool, cpu_count
+
+MAX_CORES = cpu_count()
 
 __all__ = ['stringlength', 'pdm', 'pdm2']
 
 
-def stringlength(t, x, dphi=0.1, n_periods=1000, s=0):
+def _stringlength(task):
+    """Calculates the string length for a single trial period."""
+    t, x, period = task
+    phi = ((t / period) % 1)
+    sorted_args = np.argsort(phi)
+    phi = phi[sorted_args]
+    m = x[sorted_args]
+    ll = np.hypot(np.roll(m, -1) - m, np.roll(phi, -1) - phi).sum()
+    return ll
+
+
+def stringlength(t, x, dphi=0.1, n_periods=1000, s=0, cores=None):
     """String Length [#]_.
 
     Parameters
@@ -21,6 +35,9 @@ def stringlength(t, x, dphi=0.1, n_periods=1000, s=0):
     s: int, optional
         Standard deviation of Gaussian filter used to smooth,
         measured in samples.
+    cores: int, optional
+        The number of parallel cores to use.
+        By default it will try to use all available CPU cores.
 
     Returns
     -------
@@ -35,26 +52,46 @@ def stringlength(t, x, dphi=0.1, n_periods=1000, s=0):
        observations or 'How long is a piece of string ?'," MNRAS, June 1983.
     """
     # scale x to range from -0.25 to +0.25
-    x = (x - np.max(x)) / (2 * (np.max(x) - np.min(x))) - 0.25
+    m = (x - np.max(x)) / (2 * (np.max(x) - np.min(x))) - 0.25
     df = dphi / (np.max(t) - np.min(t))
-    periods = 1 / np.linspace(df, n_periods * df, n_periods)
-    periods.sort()
-    ell = []
-    for period in periods:
-        phi = ((t / period) % 1)
-        sorted_args = np.argsort(phi)
-        phi = phi[sorted_args]
-        m = x[sorted_args]
-        ll = np.hypot(np.roll(m, -1) - m, np.roll(phi, -1) - phi).sum()
-        ell.append(ll)
-    # TODO: consider flagging false periods for rejection
+    periods = 1 / np.linspace(n_periods * df, df, n_periods)
+    if cores is None or cores > MAX_CORES:
+        cores = MAX_CORES
+    with Pool(cores) as pool:
+        tasks = [(t, m, period) for period in periods]
+        ell = pool.map(_stringlength, tasks)
     ell = np.array(ell)
     if s > 0:
         ell = gaussian_filter1d(ell, sigma=s, truncate=3.0)
     return periods, ell
 
 
-def pdm(t, x, nb=5, nc=2, pmin=.01, pmax=10, n_periods=1000, s=0):
+def _pdm(task):
+    """Calculates the PDM theta statistic for a single trial period."""
+    t, x, nb, nc, sigma, period = task
+    m0 = nb * nc
+    phi = ((t / period) % 1)
+    sorted_args = np.argsort(phi)
+    phi = phi[sorted_args]
+    m = x[sorted_args]
+    mj = []
+    m0_good = 0
+    for k in range(m0):
+        mask = (phi >= k / m0)
+        mask &= (phi < (k + nc) / m0)
+        mask |= (phi < (k - (m0 - nc)) / m0)
+        mk = m[mask]
+        if mk.size > 1:
+            mj.append(mk)
+            m0_good += 1
+    sj = np.array([np.var(k, ddof=1) for k in mj])
+    nj = np.array([k.size for k in mj])
+    ss = np.sum((nj - 1) * sj) / (np.sum(nj) - m0_good)
+    theta = ss / sigma
+    return theta
+
+
+def pdm(t, x, nb=5, nc=2, p_min=.01, p_max=10, n_periods=1000, s=0, cores=None):
     """Phase Dispersion Minimization [#]_.
 
     Parameters
@@ -67,13 +104,16 @@ def pdm(t, x, nb=5, nc=2, pmin=.01, pmax=10, n_periods=1000, s=0):
         Number of phase bins (the default is 5).
     nc: int, optional
         Number of covers per bin (the default is 2).
-    pmin, pmax: float, optional
+    p_min, p_max: float, optional
         Minimum/maximum trial period normalized by the baseline.
     n_periods: int, optional
         Number of trial periods (the default is 1000).
     s: int, optional
         Standard deviation of Gaussian filter used to smooth,
         measured in samples.
+    cores: int, optional
+        The number of parallel cores to use.
+        By default it will try to use all available CPU cores.
 
     Returns
     -------
@@ -90,64 +130,107 @@ def pdm(t, x, nb=5, nc=2, pmin=.01, pmax=10, n_periods=1000, s=0):
     t = np.asarray(t)
     x = np.asarray(x)
     sigma = np.var(x, ddof=1)
-    t0 = t.max() - t.min()
-    theta = []
-    periods = np.linspace(pmin * t0, pmax * t0, n_periods)
-    m0 = nb * nc
-    for period in periods:
-        phi = ((t / period) % 1)
-        sorted_args = np.argsort(phi)
-        phi = phi[sorted_args]
-        m = x[sorted_args]
-        mj = []
-        for k in range(m0):
-            mask = phi >= k / m0
-            mask &= phi < (k + nc) / m0
-            mask |= phi < (k - (m0 - nc)) / m0
-            mj.append(m[mask])
-        sj = np.array([np.var(k, ddof=1) for k in mj])
-        nj = np.array([k.size for k in mj])
-        ss = np.sum((nj - 1) * sj) / (np.sum(nj) - m0)
-        theta.append(ss / sigma)
+    t0 = np.max(t) - np.min(t)
+    periods = np.linspace(p_min * t0, p_max * t0, n_periods)
+    if cores is None or cores > MAX_CORES:
+        cores = MAX_CORES
+    with Pool(cores) as pool:
+        tasks = [(t, x, nb, nc, sigma, period) for period in periods]
+        theta = pool.map(_pdm, tasks)
     theta = np.array(theta)
     if s > 0:
         theta = gaussian_filter1d(theta, sigma=s, truncate=3.0)
     return periods, theta
 
 
-def pdm2(t, x, pmin=None, pmax=None, n_periods=None, s=0,
-         oversample=10, do_subharmonic=False):
+def _pdm2(task):
+    t, m, sigma, period = task
+    phi = ((t - t[0]) / period) % 1
+    bins = np.arange(0.0, 1.0, 0.1)
+    ids = np.digitize(phi, bins)
+    unique_ids = np.unique(ids)
+    mj = []
+    good = 0
+    for i in unique_ids:
+        mask = (ids == i)
+        mi = m[mask]
+        if mi.size > 1:
+            mj.append(mi)
+            good += 1
+    sj = np.array([np.var(k, ddof=1) for k in mj])
+    nj = np.array([k.size for k in mj])
+    ss = np.sum((nj - 1) * sj) / (np.sum(nj) - good)
+    theta = ss / sigma
+    return theta
+
+
+def pdm2(t, x, p_min=None, p_max=None, n_periods=None, s=0,
+         oversample=10, do_subharmonic=False, cores=None):
+    """PDM2 algorithm described in [#]_.
+
+    Parameters
+    ----------
+    t: array-like
+        Time array.
+    x: array-like
+        Signal array.
+    p_min, p_max: float, optional
+        Minimum/maximum trial period normalized by the baseline.
+    n_periods: int, optional
+        Number of trial periods (the default is 1000).
+    s: int, optional
+        Standard deviation of Gaussian filter used to smooth,
+        measured in samples.
+    oversample: scalar, optional
+        If `p_max` is omitted, the time baseline multiplied by `oversample` will
+        be used instead.
+    do_subharmonic: bool, optional
+        Whether to perform subharmonic averaging. This option looks for a
+        significant minimum in :math:`\theta` at both the main period and its
+        double. For actual variations, both will be present. For a noise result,
+        the double period signal will not be present.
+    cores: int, optional
+        The number of parallel cores to use.
+        By default it will try to use all available CPU cores.
+
+    Returns
+    -------
+    periods: ndarray[n_periods]
+        Trial periods.
+    theta: ndarray[n_periods]
+        Phase dispersion statistic.
+
+    References
+    ----------
+    .. [#] R. F. Stellingwerf, "Period Determination of RR Lyrae Stars,"
+       RR Lyrae Stars, Metal-Poor Stars and the Galaxy, August 2011.
+    """
     t = np.asarray(t)
     x = np.asarray(x)
     sigma = np.var(x, ddof=1)
     ne = t.size
-    assert x.size == ne, "incompatible array shapes"
+    if x.size != ne:
+        raise ValueError("Incompatible array shapes")
     theta_crit = 1. - 11. / ne ** 0.8
     dt = np.median(np.diff(t))
-    t0 = t.max() - t.min()
-    thetas = []
-    if pmax is None:
-        pmax = oversample * t0
-    if pmin is None:
-        pmin = 2 * dt
+    t0 = np.max(t) - np.min(t)
+    if p_max is None:
+        p_max = oversample * t0
+    if p_min is None:
+        p_min = 2 * dt
     if n_periods is None:
-        n_periods = int((1 / pmin - 1 / pmax) * oversample * t0 + 1)
-    periods = np.linspace(pmax, pmin, n_periods)
-    for period in periods:
-        phi = ((t - t[0]) / period) % 1
-        masks = np.array([np.logical_and(phi < (b + 1) / 10, phi >= b / 10)
-                          for b in range(10)])
-        sj = np.array([np.var(x[masks[j]], ddof=1) for j in range(10)])
-        nj = masks.sum(axis=1)
-        good = nj > 1
-        ss = np.sum((nj[good] - 1) * sj[good]) / np.sum(nj[good] - 1)
-        theta = ss / sigma
-        if do_subharmonic and period <= pmax / 2 and theta < theta_crit:
-            sub_index = int((n_periods - 1) * (1 - (2 * period - pmin) / (pmax - pmin)) + 0.5)
-            theta = (theta + thetas[sub_index]) / 2
-        thetas.append(theta)
-    thetas = np.array(thetas)[::-1]
-    periods = periods[::-1]
+        n_periods = int((1 / p_min - 1 / p_max) * oversample * t0 + 1)
+    periods = np.linspace(p_min, p_max, n_periods)
+    dp = (p_max - p_min) / (n_periods - 1)
+    if cores is None or cores > MAX_CORES:
+        cores = MAX_CORES
+    with Pool(cores) as pool:
+        tasks = [(t, x, sigma, period) for period in periods]
+        thetas = pool.map(_pdm2, tasks)
+    if do_subharmonic:
+        can_average, = np.where((thetas < theta_crit) & (periods <= p_max / 2))
+        sub_indices = np.round(2 * can_average + p_min / dp).astype(int)
+        thetas[can_average] = (thetas[can_average] + thetas[sub_indices]) / 2
     if s > 0:
         thetas = gaussian_filter1d(thetas, sigma=s, truncate=3.0)
     return periods, thetas
