cd8ae9769d5b037b584c88ed5392899705e84f2d
dioph <dioph@protonmail.com>
2021-07-19 14:14:24 -0300

    update development infrastructure

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import re
 
 import setuptools
 
-with open("README.md", 'r') as f:
+with open("README.md", "r") as f:
     long_description = f.read()
 
 version = re.search(
-    '^__version__\\s*=\\s*"(.*)"',
-    open("periodicity/__init__.py").read(),
-    re.M
+    '^__version__\\s*=\\s*"(.*)"', open("src/periodicity/__init__.py").read(), re.M
 ).group(1)
 
 install_requires = [
@@ -17,15 +18,15 @@ install_requires = [
     "celerite",
     "emcee >= 3.0",
     "george",
-    "numpy",
+    "matplotlib",
     "PyWavelets >= 0.5",
     "scipy >= 1.1",
-    "tqdm"
+    "tqdm",
 ]
 
 extras_require = {
-    "test": ["pytest"],
-    "docs": ["jupyter", "numpydoc", "pandoc", "sphinx_rtd_theme"]
+    "test": ["black==20.8b1", "flake8", "isort", "pytest", "pytest-cov", "tox"],
+    "docs": ["jupyter", "numpydoc", "myst-nb<0.11", "sphinx_rtd_theme"],
 }
 
 setuptools.setup(
@@ -38,7 +39,8 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/dioph/periodicity",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages("src"),
+    package_dir={"": "src"},
     install_requires=install_requires,
     extras_require=extras_require,
     classifiers=[
