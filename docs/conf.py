5c269267d43e4fc274514c9d0670138afba9313f
dioph <dioph@protonmail.com>
2026-05-12 16:33:28 -0300

    update docs config

--- docs/conf.py
+++ docs/conf.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from pkg_resources import get_distribution
+from importlib.metadata import version
 
-__version__ = get_distribution("periodicity").version
+__version__ = version("periodicity")
 
 # -- General configuration ------------------------------------------------
 
@@ -57,7 +57,7 @@ release = __version__
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -83,7 +83,7 @@ html_theme = "pydata_sphinx_theme"
 # documentation.
 #
 # html_theme_options = {}
-jupyter_execute_notebooks = "off"
+nb_execution_mode = "off"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
